bc681db002c2b44ca6a546f3bf228956379b28c9
Zak Cabric <cabricz@gmail.com>
2018-02-04 15:29:00 -0500

    add signup validation and login function

--- main.py
+++ main.py
@@ -3,9 +3,33 @@ import cgi
 from models import User, Blog
 from app import db, app
 
+def isvalid(text):
+    if 2 < len(text) < 21:
+        return True
+    else:
+        return False
+
 @app.route("/")
 def index():
     return redirect("/blog")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session["username"] = username
+            flash("Logged in", "bold")
+            print(session)
+            return redirect("/newpost")
+        else:
+            flash("Login failed", "error")
+
+    return render_template("login.html")
+
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
@@ -14,19 +38,45 @@ def signup():
         password = request.form["password"]
         verify = request.form["verify"]
 
-        #validate user input
+        username_error = ""
+        password_error = ""
+        verify_error = ""
 
-        new_user = User(username, password)
-        db.session.add(new_user)
-        db.session.commit()
-        session["username"] = username
+        existing_user = User.query.filter_by(username=username).first()
 
-        return redirect("/newpost")
-    
+        if len(username) == 0:
+            username_error = "Please enter username."
+        else:
+            if existing_user:
+                username_error = "Username already exists."
+            else:
+                if not isvalid(username):
+                    username_error = "Username must be between 3 and 20 characters long."
+        
+        if len(password) == 0:
+            password_error = "Please enter password."
+        else:
+            if not isvalid(password):
+                password_error = "Password must be betweeen 3 and 20 characters long."
+        
+        if len(verify) == 0:
+            verify_error = "Plese enter password verification."
+        else:
+            if not password == verify:
+                verify_error = "Passwords must match."
+
+        if not username_error and not password_error and not verify_error:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session["username"] = username
+            flash("Welcome " + username + ". Thanks for signing up!", "bold")
+            return redirect("/newpost")
+        else:
+            return render_template("signup.html", username=username, password=password, verify=verify, username_error=username_error, password_error=password_error, verify_error=verify_error)
+
     else:
         return render_template("signup.html")
-
-
 
 @app.route("/blog")
 def blog():
